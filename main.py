00535f7f7db77169b8a2d58f5e15fdfe84b4d440
AMOGH NAVADE <amoghmanoranjith@gmail.com>
2025-04-03 12:02:25 +0530

    restored main and created some additional files

---
+++ main.py
@@ -0,0 +1,17 @@
+[33mcommit 1214b4b4f17df6c9928f0cf81840147a56de264f[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: AMOGH NAVADE <amoghmanoranjith@gmail.com>
+Date:   Thu Apr 3 11:53:06 2025 +0530
+
+    move to main
+
+ main.py | 34 [31m----------------------------------[m
+ 1 file changed, 34 deletions(-)
+
+[33mcommit 3131a3c95262100def2fec672b6429a934b663e7[m
+Author: AMOGH NAVADE <amoghmanoranjith@gmail.com>
+Date:   Thu Apr 3 11:49:14 2025 +0530
+
+    creation
+
+ main.py | 34 [32m++++++++++++++++++++++++++++++++++[m
+ 1 file changed, 34 insertions(+)
